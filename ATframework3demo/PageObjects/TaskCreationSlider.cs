cb6b6e6f7fb2a99ff68c27c8254e5d9c2b1f3a2a
DESKTOP-RT8U219\admin <iutmow.m@gmail.com>
2022-05-31 00:16:04 +0200

    Add project files.

---
+++ ATframework3demo/PageObjects/TaskCreationSlider.cs
@@ -0,0 +1,55 @@
+﻿using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+
+namespace ATframework3demo.PageObjects
+{
+    public class TaskCreationSlider
+    {
+        internal TaskCreationSlider GiveNameToTask(Bitrix24Task task)
+        {
+
+            //ввести название задачи
+            var giveNameToTask = new WebItem("//input[@data-bx-id='task-edit-title']", "Текстбокс названия задачи");
+            giveNameToTask.Click();
+            giveNameToTask.SendKeys(task.Title);
+            return new TaskCreationSlider();
+        }
+
+        internal TaskCreationSlider GiveDescriptionToTask(Bitrix24Task task)
+        {
+            //ввести описание задачи
+            var editorFrame = new WebItem("//iframe[@class='bx-editor-iframe']", "Фрейм редактора текста");
+            editorFrame.SwitchToFrame();
+            var giveDescriptionToTask = new WebItem("//body[@contenteditable='true']", "Текстбокс описания задачи");
+            giveDescriptionToTask.SendKeys(task.Description);
+            var sliderFrame = new WebItem("//iframe[@class='side-panel-iframe']", "Фрейм слайдера");
+            DriverActions.SwitchToDefaultContent();
+            sliderFrame.SwitchToFrame();
+            return new TaskCreationSlider();
+        }
+
+        internal TaskCreationSlider OpenCreatedTask(Bitrix24Task task)
+        {
+            DriverActions.SwitchToDefaultContent();
+            var openCreatedTask = new WebItem($"//a[contains(text(), '{task.Title}') and contains(@class, 'task-title')]",
+                $"Ссылка на задачу '{task.Title}' в гриде");
+            openCreatedTask.WaitElementDisplayed();
+            openCreatedTask.Click();
+            return new TaskCreationSlider();
+        }
+
+        internal TaskCreationSlider SetTask()
+        {
+            //клик в Поставить задачу
+            var btnSetTask = new WebItem("//button[@data-bx-id='task-edit-submit' and @class='ui-btn ui-btn-success']", "Кнопка Поставить задачу");
+            btnSetTask.Click();
+            return new TaskCreationSlider();
+
+
+            //"//button[@data-bx-id='task-edit-submit' and @class='ui-btn ui-btn-success']"
+        }
+
+        
+    }
+        
+}
---
+++ ATframework3demo/TestCases/Case_Bitrix24_CreatingTask.cs
@@ -0,0 +1,49 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+
+namespace ATframework3demo.TestCases
+{
+    public class Case_Bitrix24_CreatingTask : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Проверка создания задачи", homePage => CreatingTask(homePage)));
+            return caseCollection;
+        }
+
+        void CreatingTask(atFrameWork2.PageObjects.PortalHomePage homePage)
+        {
+            var task = new Bitrix24Task("Задача для автотеста " + DateTime.Now.Ticks) { Description = "Описание задачи для автотеста" + +DateTime.Now.Ticks };
+            //перейти в Задачи и Проекты
+            homePage.LeftMenu
+                .OpenTasks()
+                //клик в Добавить задачу
+                .AddTaskBtn()
+                //ввести название задачи
+                .GiveNameToTask(task)
+                //ввести описание задачи
+                .GiveDescriptionToTask(task)
+                //клик в Поставить задачу
+                .SetTask()
+                //открыть созданную задачу
+                .OpenCreatedTask(task);
+                //добавить комментарий с ответственным
+                //.AddComment();
+                //проверить название, описание задачи и комментарий к ней
+                //.CheckTask(task);
+
+
+                
+                
+                
+                
+
+
+
+        }
+
+     
+    }
+}
